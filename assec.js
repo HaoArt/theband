e7c6d53f8d6273d9eea0dce555e684a6fd1a715c
HaoArt <124483482+HaoArt@users.noreply.github.com>
2023-06-25 20:51:15 +0700

    Update assec.js

--- assec.js
+++ assec.js
@@ -1,11 +1,11 @@
 
-const buyBtns = document.querySelectorAll('.js-buy-ticket')
+const buyBtns = document.querySelectorAll('.js-buy-ticket');
 
-const modal = document.querySelector('.js-modal')
+const modal = document.querySelector('.js-modal');
 
-const modalClose = modal.querySelector('.js-close-modal')
+const modalClose = modal.querySelector('.js-close-modal');
 
-const modalContainer= document.querySelector('.js-modal-container')
+const modalContainer= document.querySelector('.js-modal-container');
 
 // hiển thị modal (hàm hiển thị)
 function showBuyTickets(){
@@ -29,3 +29,38 @@ modal.addEventListener('click', hideBuyTickets)
 modalContainer.addEventListener('click', function(event){
     event.stopPropagation()
 })
+
+var header = document.getElementById('header');
+
+var mobileMenu = document.getElementById('js-mobile-menu');
+
+var headerHeight = header.clientHeight;
+// đóng mở menu 
+mobileMenu.onclick = function(){
+    var isClose = header.clientHeight === headerHeight;
+    if (isClose){
+        header.style.height = 'auto';
+    }
+    else{
+        header.style.height = null ;
+    }
+}
+
+// tự động đsong menu sau khi click
+
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    
+    // nextElementSibling : kiểm tra anh em liền kề của nó
+    menuItem.onclick=function(event){
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            event.preventDefault();
+        }
+        else{
+            header.style.height =null;
+        }
+    }
+}
